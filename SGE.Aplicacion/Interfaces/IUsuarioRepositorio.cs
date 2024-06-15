983a1eb3e615d4afebe0ab1166f7b14ff7df387b
smendest <smendestello@gmail.com>
2024-06-15 18:11:53 -0300

    clean user actions

--- SGE.Aplicacion/Interfaces/IUsuarioRepositorio.cs
+++ SGE.Aplicacion/Interfaces/IUsuarioRepositorio.cs
@@ -3,9 +3,5 @@
 public interface IUsuarioRepositorio
 {
   void AltaDeUsuario(Usuario usuario);
-  // void EliminarExpediente(int id);
-  // void ModificarExpediente(Expediente expedienteModificado);
-  // Expediente GetExpedienteById(int id);
-  // List<Expediente> ConsultarTodos();
 
 }
