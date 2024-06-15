28284b7191a123bbc75e1bf80a9aab6a1b37fd79
smendest <smendestello@gmail.com>
2024-06-15 16:24:13 -0300

    clean comments

--- SGE.Aplicacion/Entidades/Expediente.cs
+++ SGE.Aplicacion/Entidades/Expediente.cs
@@ -2,15 +2,6 @@
 
 public class Expediente
 {
-  /*
-  Atributos:
-  - El Id del ¿trámite?, un identificador numérico único
-  - La carátula, texto ingresado por el usuario
-  - La fecha y hora de creación
-  - La fecha y hora de la última modificación
-  - El usuario que realizó la última modificación, identificado por su Id de usuario
-  - El estado del expediente.
-  */
   public int Id { get; set; }
   public string? Caratula { get; set; }
   public DateTime FechaCreacion { get; set; }
--- SGE.Aplicacion/Entidades/Tramite.cs
+++ SGE.Aplicacion/Entidades/Tramite.cs
@@ -2,16 +2,6 @@
 
 public class Tramite
 {
-  /*
-    - El Id del trámite, identificador numérico único entre todos los trámites gestionados por el sistema
-    - El Id del expediente al que pertenece, propiedad ExpedienteId mencionada previamente
-    - La etiqueta que identifica el tipo de trámite
-    - El contenido específico del trámite, texto ingresado por el usuario
-    - La fecha y hora de creación del trámite
-    - La fecha y hora de la última modificación del trámite
-    - El usuario que realizó la última modificación, identificado por su Id de usuario.
-  */
-
   public int Id { get; set; }
   public string? Contenido { get; set; }
   public EtiquetaTramite Etiqueta { get; set; }
