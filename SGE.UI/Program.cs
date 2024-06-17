2014529b1f72d9b5d47af8d36acf2d4b08245406
smendest <smendestello@gmail.com>
2024-06-17 20:10:30 -0300

    feat: first UI components

--- SGE.UI/Program.cs
+++ SGE.UI/Program.cs
@@ -1,6 +1,38 @@
 using SGE.UI.Components;
 
+//agregamos estas directivas using
+using SGE.Repositorios;
+using SGE.Aplicacion;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//agregamos estos servicios al contenedor DI
+// Casos de uso
+builder.Services.AddTransient<CasoDeUsoExpedienteAlta>();
+builder.Services.AddTransient<CasoDeUsoExpedienteBaja>();
+builder.Services.AddTransient<CasoDeUsoExpedienteConsultaPorId>();
+builder.Services.AddTransient<CasoDeUsoExpedienteConsultaTodos>();
+builder.Services.AddTransient<CasoDeUsoExpedienteModificacion>();
+builder.Services.AddTransient<CasoDeUsoTramiteAlta>();
+builder.Services.AddTransient<CasoDeUsoTramiteBaja>();
+builder.Services.AddTransient<CasoDeUsoTramiteConsultaPorEtiqueta>();
+builder.Services.AddTransient<CasoDeUsoTramiteModificacion>();
+builder.Services.AddTransient<CasoDeUsoUsuarioAlta>();
+builder.Services.AddTransient<CasoDeUsoUsuarioAutenticar>();
+builder.Services.AddTransient<CasoDeUsoUsuarioBaja>();
+builder.Services.AddTransient<CasoDeUsoUsuarioModificacion>();
+builder.Services.AddScoped<IExpedienteRepositorio, RepositorioExpedientes>();
+builder.Services.AddScoped<ITramiteRepositorio, RepositorioTramites>();
+builder.Services.AddScoped<IUsuarioRepositorio, RepositorioUsuario>();
+
+// Dependendias de los casos de uso
+builder.Services.AddTransient<ExpedienteValidador>();
+builder.Services.AddTransient<TramiteValidador>();
+builder.Services.AddScoped<IServicioAutorizacion, ServicioAutorizacion>();
+builder.Services.AddScoped<IEspecificacionCambioEstado, EspecificacionCambioEstado>();
+builder.Services.AddScoped<IServicioActualizacionEstado, ServicioActualizacionEstado>();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
