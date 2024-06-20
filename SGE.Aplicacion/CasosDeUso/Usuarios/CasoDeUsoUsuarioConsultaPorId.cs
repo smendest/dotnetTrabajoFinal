38ef0fda159937c11256b9f8035a3ed7d9c0cc44
smendest <smendestello@gmail.com>
2024-06-20 18:47:40 -0300

    feat: new pages and improvement

---
+++ SGE.Aplicacion/CasosDeUso/Usuarios/CasoDeUsoUsuarioConsultaPorId.cs
@@ -0,0 +1,14 @@
+﻿namespace SGE.Aplicacion;
+
+public class CasoDeUsoUsuarioConsultaPorId(
+  IUsuarioRepositorio repoUsuario
+  )
+{
+  public Usuario Ejecutar(int idUsuario)
+  {
+
+    return repoUsuario.GetUserById(idUsuario);
+
+  }
+
+}
